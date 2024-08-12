02605e4eb73f45a682c829f365e9743f5fb83e75
mali84211 <mali84211@gmail.com>
2024-08-12 14:50:41 +0300

    V2

--- C42-G02-LINQ01/Program.cs
+++ C42-G02-LINQ01/Program.cs
@@ -10,24 +10,32 @@
             ////var x;
             //var x = null;
             //Data1 = 12;
-           //dynamic Data2 = "ahmed";
-           // Console.WriteLine(Data2.GetType().Name);
-           // Data2 = 12;
-           // Console.WriteLine(Data2.GetType().Name);
-           // Data2 = 'a';
-           // Console.WriteLine(Data2.GetType().Name);
-           // Data2 = 1.2;
-           // Data2 = new StringBuilder(Value:"");
+            //dynamic Data2 = "ahmed";
+            // Console.WriteLine(Data2.GetType().Name);
+            // Data2 = 12;
+            // Console.WriteLine(Data2.GetType().Name);
+            // Data2 = 'a';
+            // Console.WriteLine(Data2.GetType().Name);
+            // Data2 = 1.2;
+            // Data2 = new StringBuilder(Value:"");
 
-           // Console.WriteLine(Data2.GetType().Name);
+            // Console.WriteLine(Data2.GetType().Name);
             //dynamic x = null;
             //Console.WriteLine(x);
             #endregion
 
-            #region
+            #region V2
+            //int number = 12345;
+            ////var Result=InExtension.Reverse(number);
+            ////Console.WriteLine(Result);
+            //number.Reverse();
+            //var Result = number.Reverse();
+            //Console.WriteLine(Result);
+            //var x = 11.2312 % 10;
+            //Console.WriteLine(x);
             #endregion
 
-            #region
+            #region 
             #endregion
 
             #region
